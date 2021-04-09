92d26e174d18ba21c9ace45d78982f50a997e70e
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-04-08 22:45:54 -0500

    feat: Initial commit

---
+++ src/main/java/com/rsm/rsmtest/RsmTestApplication.java
@@ -0,0 +1,13 @@
+package com.rsm.rsmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsm/rsmtest/RsmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsm.rsmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
